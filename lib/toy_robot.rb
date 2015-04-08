5bfb58d4cedf7652657fd27fc7df12d50b4dc27e
Miheer Salunke <miheer.salunke@gmail.com>
2015-04-09 01:07:34 +0530

    Cleanup Added exception

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -15,19 +15,54 @@ module ToyRobot
       @direction = direction 
     end
 
-    def self.check_feasibility(x, y)
-      x <= MAX_X && x >= 0 && y <= MAX_Y && y >= 0 ? true : false
+    def self.get_result(command)
+      get_position_and_direction(*command)
+    end             
+
+    def self.get_position_and_direction(command = nil, x = nil, y = nil, direction = nil)
+      case command
+      when 'PLACE'
+        @toy_robot = ToyRobot::Bot.place(command, x , y, direction) if x and y and direction
+      when "MOVE","EXIT","HELP"
+        @toy_robot.send(command.downcase) if @toy_robot #and @x and @y and @direction
+      when 'LEFT'
+        @toy_robot.turn LEFT if @toy_robot #and @x and @y and @direction
+      when 'RIGHT'
+        @toy_robot.turn RIGHT if @toy_robot #and @x and @y and @direction
+      when 'REPORT'
+        puts "report"
+        puts @toy_robot.report if @toy_robot #and @x and @y and @direction
+      end
     end
 
-    def turn(direction)
-      index = DIRECTIONS.index(@direction)
-      @direction = DIRECTIONS.rotate!(direction)[index]
+    def self.check_feasibility(x, y)
+      x <= MAX_X && x >= 0 && y <= MAX_Y && y >= 0 ? true : false
     end
 
     def self.place(command, x, y, direction)
       if self.check_feasibility x.to_i , y.to_i
         @toy_robot = Bot.new(command, x, y, direction) if x && y
       end
+    end
+
+    def move
+      case @direction
+      when 'WEST'
+        @x = (@x.to_i - 1).to_s if ToyRobot::Bot.check_feasibility(@x.to_i - 1, @y.to_i)
+      when 'EAST'
+        @x = (@x.to_i + 1).to_s  if ToyRobot::Bot.check_feasibility(@x.to_i + 1, @y.to_i)
+      when 'NORTH'
+        @y = (@y.to_i + 1).to_s if ToyRobot::Bot.check_feasibility(@x.to_i, @y.to_i + 1)
+      when 'SOUTH'
+        @y = (@y.to_i - 1).to_s  if ToyRobot::Bot.check_feasibility(@x.to_i, @y.to_i - 1)
+      else
+        nothing
+      end
+    end
+
+    def turn(direction)
+      index = DIRECTIONS.index(@direction)
+      @direction = DIRECTIONS.rotate!(direction)[index]
     end
 
     def exit
@@ -43,41 +78,6 @@ module ToyRobot
         "REPORT show position of bot with the direction it is facing\n"\
         "EXIT\n"\
         "*********************************\n"
-    end
-
-    def self.get_position_and_direction(command = nil, x = nil, y = nil, direction = nil)
-      case command 
-        when 'PLACE'
-          @toy_robot = ToyRobot::Bot.place(command, x , y, direction) if x and y and direction
-        when "MOVE","EXIT","HELP" 
-         @toy_robot.send(command.downcase) if @toy_robot #and @x and @y and @direction
-        when 'LEFT' 
-          @toy_robot.turn LEFT if @toy_robot #and @x and @y and @direction
-        when 'RIGHT'
-          @toy_robot.turn RIGHT if @toy_robot #and @x and @y and @direction
-        when 'REPORT'
-          puts "report"
-        puts @toy_robot.report if @toy_robot #and @x and @y and @direction
-      end
-    end
-
-    def self.get_result(command)
-      get_position_and_direction(*command)
-    end
-
-    def move 
-      case @direction
-      when 'WEST'
-        @x = (@x.to_i - 1).to_s if ToyRobot::Bot.check_feasibility(@x.to_i - 1, @y.to_i)
-      when 'EAST'
-        @x = (@x.to_i + 1).to_s  if ToyRobot::Bot.check_feasibility(@x.to_i + 1, @y.to_i)
-      when 'NORTH'
-        @y = (@y.to_i + 1).to_s if ToyRobot::Bot.check_feasibility(@x.to_i, @y.to_i + 1)
-      when 'SOUTH'
-        @y = (@y.to_i - 1).to_s  if ToyRobot::Bot.check_feasibility(@x.to_i, @y.to_i - 1)
-      else
-        #nothing
-      end
     end
 
     def report
